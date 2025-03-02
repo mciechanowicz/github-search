6967d65b1c724f69dd0117ca13d54796cfcba8d0
Michał <michal.ciechanowicz@stermedia.eu>
2025-03-02 22:05:07 +0100

    fix: imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,17 @@
+import type { Metadata } from 'next';
+import { Roboto } from 'next/font/google';
+
+import { ThemeProvider } from '@mui/material/styles';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+
 import { QueryProvider } from '@/providers/QueryProvider';
+
+import theme from '../../theme';
+
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
-import { ThemeProvider } from '@mui/material/styles';
-import type { Metadata } from 'next';
-import { Roboto } from 'next/font/google';
-import theme from '../../theme';
 import './globals.css';
 
 const roboto = Roboto({
