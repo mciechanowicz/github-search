9ce0a62bd48965ed28f322b8a0ef6ec9b7d602c5
Michał <michal.ciechanowicz@stermedia.eu>
2025-03-02 19:28:10 +0100

    feat: add translations

---
+++ src/translations.ts
@@ -0,0 +1,15 @@
+export const translations = {
+  common: {
+    retry: 'Try again',
+  },
+  userSearch: {
+    title: ' GitHub User Search',
+    textFieldUsernameRequired: 'Username is required',
+    textFieldUsernameMin: 'Username must be at least 3 characters',
+    textFieldLabel: 'Search username',
+  },
+  userList: {
+    emptyList: 'No users found!',
+    linkTitle: 'View Profile',
+  },
+};
