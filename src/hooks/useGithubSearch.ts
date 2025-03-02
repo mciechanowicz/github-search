da13dabceae4dc97d6c8c92a5c98e222bec94e8e
Michał <michal.ciechanowicz@stermedia.eu>
2025-03-02 22:00:24 +0100

    feat: implement mechanism to pause requests if the requests limit was reached

--- src/hooks/useGithubSearch.ts
+++ src/hooks/useGithubSearch.ts
@@ -1,12 +1,16 @@
+import { useState } from 'react';
+
 import { useInfiniteQuery } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
 
 import { getUsers, RESULTS_PER_PAGE } from '@/services/getUsers';
 import { GitHubSearchResponse, User } from '@/types/api';
 
 const useGitHubSearch = (username: string) => {
-  return useInfiniteQuery<
+  const [isQueryLimitReached, setIsQueryLimitReached] = useState(false);
+  const query = useInfiniteQuery<
     GitHubSearchResponse,
-    Error,
+    AxiosError,
     User[],
     readonly ['users', string],
     number
@@ -15,6 +19,7 @@ const useGitHubSearch = (username: string) => {
     queryFn: ({ pageParam = 1 }) => getUsers(username, pageParam),
     enabled: !!username,
     getNextPageParam: (lastPage, allPages) => {
+      if (isQueryLimitReached) return undefined;
       const maxPages = Math.ceil(lastPage.total_count / RESULTS_PER_PAGE);
       const nextPage = allPages.length + 1;
       return nextPage <= maxPages ? nextPage : undefined;
@@ -22,7 +27,26 @@ const useGitHubSearch = (username: string) => {
     // Info for the recruiter: technique related to functional programming (usage of flatMap)
     select: (data) => data.pages.flatMap((page) => page.items),
     initialPageParam: 1,
+    refetchOnWindowFocus: false,
+    retry: (failureCount, error) => {
+      if (error?.response?.status === 403) {
+        setIsQueryLimitReached(true);
+        return false;
+      }
+      return failureCount < 3;
+    },
   });
+
+  const handleRetry = () => {
+    setIsQueryLimitReached(false);
+    query.refetch();
+  };
+
+  return {
+    ...query,
+    hasNextPage: query.hasNextPage && !isQueryLimitReached,
+    handleRetry,
+  };
 };
 
 export default useGitHubSearch;
